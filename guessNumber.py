aa60771adf47d7c2bf5f989d30699e24b2985f6b
amro <amgr948@gmail.com>
2026-03-06 14:56:46 +0200

    first commit

---
+++ guessNumber.py
@@ -0,0 +1,21 @@
+import random
+import os
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+GuessNumber = random.randint(1,100)
+tries=0
+clear()
+print ("\n🎲 Welcome to the Number Guessing Game!\n")
+while (True):
+    your_number = int(input("Guess a number between 1 and 100 : "))
+    tries +=1
+    if (GuessNumber > your_number):
+        print(" Too low! Try again.")
+    elif(GuessNumber < your_number):
+        print(" Too big ! Try again.")
+    else:
+        print(f"\n🎉🎉🎉 Correct! You guessed the number in {tries} tries.")
+        break
+
